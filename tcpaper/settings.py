195a87faa1ca51c5e39c682da5611c1e2b494d63
Wesley Chow <wesleychow5@gmail.com>
2014-10-15 21:55:44 -0700

    Constructed correct xpath expressions to extract data for a TCArticleItem

---
+++ tcpaper/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for tcpaper project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'tcpaper'
+
+SPIDER_MODULES = ['tcpaper.spiders']
+NEWSPIDER_MODULE = 'tcpaper.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'tcpaper (+http://www.yourdomain.com)'
